d78efd5590160b50d62a3cb73aaf42e5cd318d18
Daniel Neal <danielneal2@gmail.com>
2024-01-03 12:46:15 +0000

    Update api key

--- personal/gptel.el
+++ personal/gptel.el
@@ -1,6 +1,6 @@
 (straight-use-package 'gptel)
 ;; set configurations
-(setq gptel-api-key "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")
+(setq gptel-api-key "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")
 (setq gptel-model "gpt-4-1106-preview")
 (setq gptel--system-message
       "You are a helpful assistant in Emacs.
